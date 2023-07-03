077bcf587a34fce13992909e0b6905191a82dd2e
Eugenio <lugezz@gmail.com>
2023-07-03 20:02:22 -0300

    Login y paquetes

--- books_app/src/main.jsx
+++ books_app/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { login } from '../tools/httpClient.js'
+
+login('grupo7', 'grupo7_comision23307')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />

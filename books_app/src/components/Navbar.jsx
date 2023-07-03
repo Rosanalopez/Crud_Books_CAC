44969f7ad6fc02a65dfc2fa98266c2dd4b78f0bb
m-gaia <gaia.marianela@gmail.com>
2023-07-03 18:06:00 -0300

    navbar

--- books_app/src/components/Navbar.jsx
+++ books_app/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { ImBooks } from "react-icons/im";
 import { HiOutlineUser, HiOutlineShoppingCart, HiOutlineMenuAlt3, HiOutlineX } from "react-icons/hi";
